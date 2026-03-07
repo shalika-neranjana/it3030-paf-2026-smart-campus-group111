abde51b11f1a96e13c4fcab51e195b6547580829
Prabhashi Buddhika <buddhikaprabhashi11@gmail.com>
2026-03-07 10:00:00 +0530

    Implement OAuth 2.0 Google Sign-in

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.jsx'
+
+const GOOGLE_CLIENT_ID = "757745989563-gtfdp78m4u1nocd7a348lu50cr3f5fsm.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
